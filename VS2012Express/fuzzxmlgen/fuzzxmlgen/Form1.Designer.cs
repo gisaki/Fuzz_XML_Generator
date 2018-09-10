6ebfc4395df5428366558aca3c0ea10e19ec80a8
gisaki <gisaki@gmail.com>
2018-09-10 22:56:34 +0900

    First Commit

---
+++ VS2012Express/fuzzxmlgen/fuzzxmlgen/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace fuzzxmlgen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 91);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(260, 159);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ VS2012Express/fuzzxmlgen/fuzzxmlgen/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace fuzzxmlgen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 
+        // Drop
+        //
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            // Get the dropped filenames
+            string[] fileNames = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            // TODO
+            // string xmltext = File.ReadAllText(fileNames[0], Encoding.Default);
+            // textBox1.Text = xmltext;
+
+            // XML Parse
+            textBox1.Text = ParseXML(fileNames[0]);
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            // check if files or not
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] drags = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string d in drags)
+                {
+                    // if not files, reject all
+                    if (!System.IO.File.Exists(d))
+                    {
+                        return;
+                    }
+                }
+                // If all are files and exist, accept them
+                e.Effect = DragDropEffects.Copy;
+            } 
+        }
+
+        // 
+        // XML
+        // 
+        private int depth_ = 0;
+        private string ParseXML(string filename)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(filename);
+
+            return findAllNodes(xmlDocument);
+        }
+        private string findAllNodes(XmlNode node)
+        {
+            string ret = "";
+            this.depth_++;
+
+            Console.WriteLine("node.NodeType = [" + node.NodeType + "]");
+            Console.WriteLine("node.Name = [" + node.Name + "]");
+            Console.WriteLine("node.Value = [" + node.Value + "]");
+
+            ret += printNodeTagStart(node);
+            foreach (XmlNode n in node.ChildNodes)
+            {
+                ret += findAllNodes(n);
+            }
+            ret += printNodeTagEnd(node);
+            this.depth_--;
+            return ret;
+        }
+        private string printNodeTagStart(XmlNode node)
+        {
+            string ret = "";
+            // 開始タグ
+            switch (node.NodeType)
+            {
+                case XmlNodeType.Document:
+                    /* nop */
+                    break;
+                case XmlNodeType.XmlDeclaration:
+                    ret += "<?" + node.Name + " " + node.Value + "?>";
+                    break;
+                case XmlNodeType.Element:
+                    ret += "\r\n";
+                    ret += new string(' ', this.depth_ * 4);
+                    ret += "<" + node.Name;
+                    if (node.Attributes != null)
+                    {
+                        foreach (XmlNode attr in node.Attributes)
+                        {
+                            Console.WriteLine("attr.Name = [" + attr.Name + "]");
+                            Console.WriteLine("attr.Value = [" + attr.Value + "]");
+                            ret += " " + attr.Name;
+                            ret += "=" + "\"" + attr.Value + "\"";
+                        }
+                    }
+                    ret += ">";
+                    break;
+                case XmlNodeType.Text:
+                    ret += node.Value;
+                    break;
+                default:
+                    break;
+            }
+            return ret;
+        }
+        private string printNodeTagEnd(XmlNode node)
+        {
+            string ret = "";
+            // 終了タグ
+            switch (node.NodeType)
+            {
+                case XmlNodeType.Element:
+                    /* nop */
+                    ret += "</" + node.Name;
+                    ret += ">";
+                    break;
+                default:
+                    break;
+            }
+            return ret;
+        }
+    }
+}
